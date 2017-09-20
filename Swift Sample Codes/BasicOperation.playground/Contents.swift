7bebf082235e4aae72dcc3ee395f4d02985b1ee7
Hasib Iqbal Pranto <pranto@Hasibs-iMac.local>
2017-09-20 17:10:57 +0600

    BasicOperation Added

---
+++ Swift Sample Codes/BasicOperation.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+import Darwin
+import Cocoa
+
+print("5 + 4 = \(5 + 4)")
+print("5 - 4 = \(5 - 4)")
+print("5 * 4 = \(5 * 4)")
+print("5.0 / 4.0 = \(5.0 / 4.0)")
+print("5 % 4 = \(5 % 4)")
+
+print("4 + 5 * 10 = \(4 + 5 * 10)")
+print("(4 + 5) * 10 = \((4 + 5) * 10)")
+
+var randNum: Int = 5
+randNum += 1
+print(randNum)
+randNum -= 1
+print(randNum)
+
+
+// Use of Different Aggregate Functions
+// There is also sin, cos, tan, asin, acos, atan, sinh, cosh, tanh
+
+print("abs(-5) :\(abs(-5))")
+print("floor(5.5) : \(floor(5.5))")
+print("ceil(5.5) : \(ceil(5.4))")
+print("max(5,4) :\(max(5,4))")
+print("min(5,4) :\(min(5,4))")
+print("pow(5,2) :\(pow(5,2))")
+print("sqrt(25) :\(sqrt(25))")
+print("log(2.718) = \(log(2.718) )")
+
