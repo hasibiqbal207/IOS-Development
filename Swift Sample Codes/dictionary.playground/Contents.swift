6a02e2f3b4086b007067b0f2361c9fb2441366b8
Hasib Iqbal Pranto <pranto@Hasibs-iMac.local>
2017-09-21 01:14:15 +0600

    Dictionary & Tuples Added

--- Swift Sample Codes/dictionary.playground/Contents.swift
+++ Swift Sample Codes/dictionary.playground/Contents.swift
@@ -4,8 +4,66 @@ import Cocoa
 import Foundation
 
 
+// Syntax for declaring Dictonary
 let measurements = ["bedroom" : 150, "kithcen" : 120, "living" : 300, "bathroom" : 230]
 
 let details = ["name" : "John Doe"]
+
+let measurements1: [String : Int] = ["bedroom" : 150, "kithcen" : 120, "living" : 300, "bathroom" : 230]
+
+// Declaring Empty Dictionary
+var measurements2: [String : Int] = [:]
+    
+measurements2 = [
+    "bedroom" : 150,
+    "kithcen" : 120,
+    "living" : 300,
+    "bathroom" : 230]
+
+var measurements3 = [1 : "A", 2 : "B" ]
+
+// Retriving data from Dictionary
+measurements2["bedroom"]
+
+measurements3[1]
+
+// Adding New Key Pair Value
+measurements3[3] = "C"
+
+measurements3
+
+type(of : measurements3)
+
+// Update Value
+
+let checkvalue = measurements3.updateValue("pranto", forKey: 3)    // It return its previous value
+
+measurements3.updateValue("Joarder", forKey: 4)   // Doesn't throw any error, it'll simply add this to the dictionary
+
+measurements3
+
+// Removing value from dictionary
+
+measurements3
+
+measurements3.removeValue(forKey: 1)
+measurements3[2] = nil
+
+measurements3
+
+// Useful Dictionary methods
+
+measurements3.keys
+measurements3.values
+
+// Conversion of dictionary to array
+var measurekeys = Array(measurements3.keys)
+var measurevalues = Array(measurements3.values)
+
+// Print Dictionary using loop
+
+for (key,value) in measurements3 {
+    print("\(key) : \(value)")
+}
 
 
---
+++ Swift Sample Codes/tuples.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let height: Double = 6.25
+let weight: Int = 175
+
+let myData = (height, weight)
+print("Height : \(myData.0)")
+
+let myData2 = (height: 6.25, weight: 175)
+print("Weight: \(myData2.weight)")
