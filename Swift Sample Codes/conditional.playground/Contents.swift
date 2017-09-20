37e60e9ad643cc565fc78185fb02495e99ff313a
Hasib Iqbal Pranto <pranto@Hasibs-iMac.local>
2017-09-20 16:36:27 +0600

    Conditional Added

---
+++ Swift Sample Codes/conditional.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Darwin
+
+/*
+var age: Int = 8
+
+if age < 5 {
+    print("Go to Pre-school")
+}
+else if age > 5 {
+    print("Go to High-school")
+}
+else if (age > 5) && (age <= 18){
+    print("Growing Student")
+}
+else{
+    print("Go to College")
+}
+
+var income: Double = 1200
+var gpa: Double = 3.9
+
+print("Get Grant : \((income < 1500) || (gpa >= 3.8))")
+
+print("Not True : \(!true)")
+*/
+
+ // Ternary Operator
+
+var bage: Int = 18
+var canDrive: Bool = bage >= 16 ? true : false
+
+ // Switch Case
+
+let ingredient = "pizza"
+
+switch ingredient {
+    case "tomato", "pasta":
+        print("Not Today")
+        fallthrough // Works Like Break
+    
+    case "pizza":
+        print("Sause Please")
+    
+default:
+    print("Water")
+}
+
+
+let testScore: Int = 93
+
+switch testScore {
+case 93...100:
+    print("You Got A")
+default:
+    print("You Got A-")
+}
+
+
---
+++ Swift Sample Codes/datatypes.playground/Contents.swift
@@ -0,0 +1,55 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import Darwin
+
+var hello = "Hello, playground"
+
+var world: String = "Pranto"
+
+var msg = hello + " " + world
+
+print("I'm \(world)")
+
+
+// Data Types
+
+ // Constant Variable
+
+let a = 10
+
+ // Other
+
+var pi1: Int = 50
+
+var pi2: Float = 50.25
+
+var pi3: Double = 50.550
+
+var pi4: String = "Data Types"
+
+print("Min Int \(Int64.min)")
+print("Max Int \(Int64.max)")
+
+print("Min Double \(DBL_MIN)")
+print("Max Double \(DBL_MAX)")
+
+print("Max float \(Float.greatestFiniteMagnitude)")
+
+var pi5: Bool = true
+
+var pi6: Character = "p"
+
+ // Type Cast
+
+var pi7: Int = 5
+
+var pi8: Double = 3.5
+
+var pi9 = Double(pi7) + pi8
+
+print("3 : \(Int(3.24))")
+
+
+
+
